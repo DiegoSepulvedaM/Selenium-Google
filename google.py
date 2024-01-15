c186b8a6062666882e2fec5670b3121aa7290bb8
Diego <diego.sepu@outlook.es>
2024-01-15 17:59:41 -0300

    adding

--- google.py
+++ google.py
@@ -3,30 +3,29 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.keys import Keys
 import time
 
-def test_google_search():
-    chromedriver_path = "/usr/bin/chromedriver"
-    
-    # Configura el servicio de Chrome con la ruta al chromedriver
-    chrome_service = ChromeService(executable_path=chromedriver_path)
-    
-    # Configura el webdriver utilizando el servicio de Chrome
-    driver = webdriver.Chrome(service=chrome_service)
+# Establece la ruta de chromedriver
+chromedriver_path = "/usr/bin/chromedriver"
 
-    try:
-        # Realiza la búsqueda en Google
-        driver.get("https://www.google.com")
-        search_box = driver.find_element("name", "q")
-        search_box.send_keys("Jenkins")
-        search_box.send_keys(Keys.RETURN)
+# Configura el servicio de Chrome con la ruta al chromedriver
+chrome_service = webdriver.ChromeService(executable_path=chromedriver_path)
 
-        # Espera un momento para ver los resultados
-        time.sleep(2)
+# Configura el webdriver utilizando el servicio de Chrome
+driver = webdriver.Chrome(service=chrome_service)
 
-        # Captura un screenshot para verificar el resultado (opcional)
-        driver.save_screenshot("google_search_results.png")
-    finally:
-        # Cierra el navegador al finalizar
-        driver.quit()
+try:
+    # Realiza la búsqueda en Google
+    driver.get("https://www.google.com")
+    search_box = driver.find_element("name", "q")
+    search_box.send_keys("Jenkins")
+    search_box.send_keys(Keys.RETURN)
 
-# Llama a la función de prueba
-test_google_search()
+    # Espera un momento para ver los resultados
+    time.sleep(2)
+
+    # Captura un screenshot para verificar el resultado (opcional)
+    driver.save_screenshot("google_search_results.png")
+finally:
+    # Cierra el navegador al finalizar
+    driver.quit()
+
+
