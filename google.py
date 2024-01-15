a8abf55884caef3d11dfd0781de17609716e8c1d
Diego <diego.sepu@outlook.es>
2024-01-15 17:38:27 -0300

    commit

--- google.py
+++ google.py
@@ -1,32 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.keys import Keys
 import time
 
 def test_google_search():
-    chromedriver_path = "/usr/local/bin/chromedriver"
+    # Crear una instancia del controlador de Chrome
+    driver = webdriver.Chrome()
     
-    # Configura el servicio de Chrome con la ruta al chromedriver
-    chrome_service = ChromeService(executable_path=chromedriver_path)
+    # Navegar a Google
+    driver.get("https://www.google.com")
     
-    # Configura el webdriver utilizando el servicio de Chrome
-    driver = webdriver.Chrome(service=chrome_service)
+    # Esperar 2 segundos para que puedas ver la ventana del navegador
+    time.sleep(2)
+    
+    # Cerrar el navegador
+    driver.quit()
 
-    try:
-        # Realiza la búsqueda en Google
-        driver.get("https://www.google.com")
-        search_box = driver.find_element("name", "q")
-        search_box.send_keys("Jenkins")
-        search_box.send_keys(Keys.RETURN)
-
-        # Espera un momento para ver los resultados
-        time.sleep(2)
-
-        # Captura un screenshot para verificar el resultado (opcional)
-        driver.save_screenshot("google_search_results.png")
-    finally:
-        # Cierra el navegador al finalizar
-        driver.quit()
-
-# Llama a la función de prueba
+# Llamar a la función de prueba
 test_google_search()
