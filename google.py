8f7f632ff0c50513094c2aa3d7f016f874fe0d5c
Diego <diego.sepu@outlook.es>
2024-01-15 18:22:45 -0300

    git

--- google.py
+++ google.py
@@ -4,32 +4,32 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
 
-# Establece la ruta de chromedriver
-chromedriver_path = "/usr/bin/chromedriver"
+# Rutas a los ejecutables de Chrome y Chromedriver
 chrome_binary_path = "/usr/bin/google-chrome"
+chromedriver_path = "/usr/bin/chromedriver"
 
-# Configura las opciones de Chrome
-chrome_options = webdriver.ChromeOptions()
+# Configuración de opciones de Chrome
+chrome_options = Options()
 chrome_options.binary_location = chrome_binary_path
 
-# Configura el servicio de Chrome con la ruta al chromedriver
+# Configuración del servicio de Chromedriver
 chrome_service = ChromeService(executable_path=chromedriver_path)
 
-# Configura el webdriver utilizando las opciones y el servicio de Chrome
+# Configuración del WebDriver de Selenium
 driver = webdriver.Chrome(options=chrome_options, service=chrome_service)
 
 try:
-    # Realiza la búsqueda en Google
+    # Acciones del script aquí...
     driver.get("https://www.google.com")
     search_box = driver.find_element("name", "q")
     search_box.send_keys("Jenkins")
     search_box.send_keys(Keys.RETURN)
 
-    # Espera un momento para ver los resultados
+    # Esperar un momento para ver los resultados
     time.sleep(2)
 
-    # Captura un screenshot para verificar el resultado (opcional)
-    driver.save_screenshot("google_search_results.png")
+    # Puedes agregar más acciones aquí...
+
 finally:
-    # Cierra el navegador al finalizar
+    # Cerrar el navegador al finalizar
     driver.quit()
