01bd2a8d749b57c8cbfd287b5787b5814f49e720
Diego <diego.sepu@outlook.es>
2024-01-15 18:12:56 -0300

    git

--- google.py
+++ google.py
@@ -1,33 +1,30 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.keys import Keys
 import time
 
+def test_google_search():
+    chromedriver_path = "/usr/bin/chromedriver"  # Reemplaza con la ubicación correcta de chromedriver
+    chrome_binary_path = "/usr/bin/google-chrome"  # Reemplaza con la ubicación correcta de Google Chrome
 
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = chrome_binary_path
 
-# Establece la ruta de chromedriver
-chromedriver_path = "/usr/bin/chromedriver"
+    driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
 
-# Configura el servicio de Chrome con la ruta al chromedriver
-chrome_service = webdriver.ChromeService(executable_path=chromedriver_path)
+    try:
+        driver.get("https://www.google.com")
+        search_box = driver.find_element("name", "q")
+        search_box.send_keys("Jenkins")
+        search_box.send_keys(Keys.RETURN)
 
-# Configura el webdriver utilizando el servicio de Chrome
-driver = webdriver.Chrome(service=chrome_service)
+        # Espera un momento para ver los resultados
+        time.sleep(2)
 
-try:
-    # Realiza la búsqueda en Google
-    driver.get("https://www.google.com")
-    search_box = driver.find_element("name", "q")
-    search_box.send_keys("Jenkins")
-    search_box.send_keys(Keys.RETURN)
+        # Captura un screenshot para verificar el resultado (opcional)
+        driver.save_screenshot("google_search_results.png")
+    finally:
+        driver.quit()
 
-    # Espera un momento para ver los resultados
-    time.sleep(2)
-
-    # Captura un screenshot para verificar el resultado (opcional)
-    driver.save_screenshot("google_search_results.png")
-finally:
-    # Cierra el navegador al finalizar
-    driver.quit()
-
+# Llama a la función de prueba
+test_google_search()
 
